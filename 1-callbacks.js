2b8e8ede900119d76b6d259c5da4e31884911a53
Mason J. White <33262802+masonjwhite@users.noreply.github.com>
2018-11-27 22:48:06 -0500

    Update 1-callbacks.js

--- 1-callbacks.js
+++ 1-callbacks.js
@@ -3,7 +3,7 @@
     - Deals with asnychronous code
 
     - **HOW CAN I IDENTIFY IT?**
-      - Take note of the callback function declared as the second argument to the writeFile function!
+      - Take note of the callback function declared as the second argument to the readFile function!
 
     - **ERROR HANDLING?** 
       - The if (err) code block is what fires if something went wrong
